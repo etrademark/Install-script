f2a9384c3a569d260353b93a6a10866a8b770793
etrademark <etrademark.github@gmail.com>
2025-04-10 19:12:54 +0200

    add wofi and rofi to install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p "$installDir/logs"
 
 zsh="zsh"
 hyprland="hyprland noto-fonts kitty"
-dotfiles="wl-clipboard thunar blueman networkmanager network-manager-applet pulseaudio pavucontrol alsa-firmware cava waybar"
+dotfiles="rofi wofi wl-clipboard thunar blueman networkmanager network-manager-applet pulseaudio pavucontrol alsa-firmware cava waybar"
 lazyvim="neovim nodejs npm ripgrep stylua lua51 luarocks hererocks fd lazygit fzf ghostscript shfmt ast-grep"
 
 asusctl="asusctl supergfxctl rog-control-center"
